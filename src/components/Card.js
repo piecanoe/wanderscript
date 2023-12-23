65195f5fa403502312a8e99dc9a770f55f40d726
Captain Du <karenkaydu@gmail.com>
2023-12-23 12:44:16 -0800

    Add pin icon and change site title and favicon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 export default function Card(props) {
     return (
         <section className="card">
             <img className="card-photo" src={props.photo} alt="Attraction" />
             <div className="card-info">
                 <div className = "card-map">
+                    <FontAwesomeIcon icon={faLocationDot} className="pinIcon" />
                     <h2 className="card-country">{props.country}</h2>
                     <a className="google-maps" href={props.googleMapsUrl} target="_blank">View on Google Maps</a>
                 </div>
