5cd8e46f2cdd0e30fba793f516639218825a3f52
Captain Du <karenkaydu@gmail.com>
2024-01-10 10:19:13 -0800

    Replace linked images with images from folder

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ const data = [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        photo:"https://cdn.pixabay.com/photo/2023/05/22/13/45/mount-fuji-8010752_1280.jpg"
+        photo:"./imgs/mount_fuji.jpg"
     },
     {
         id: 2,
@@ -17,7 +17,7 @@ const data = [
         startDate: "12 May, 2021",
         endDate: "8 June, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
-        photo:"https://cdn.pixabay.com/photo/2014/06/06/09/36/sydney-opera-house-363244_1280.jpg"
+        photo:"./imgs/sydney_opera_house.jpg"
     },
     {
         id: 3,
@@ -27,7 +27,7 @@ const data = [
         startDate: "01 Oct, 202",
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        photo:"https://cdn.pixabay.com/photo/2023/07/05/18/13/mountains-8108961_1280.jpg"
+        photo:"./imgs/geirangerfjord.jpg"
     }
 ]
 
